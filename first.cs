2ed76546b4133b6b9d6ba1ac2aa657a71c6b1e38
Munashe <u18030948@tuks.co.za>
2021-08-04 10:59:28 +0200

    Update first.cs

--- first.cs
+++ first.cs
@@ -4,7 +4,7 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
